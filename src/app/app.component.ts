18a9dfe867fff10bb57b375ab5256e651e7b98ec
iras.users@gmail.com <iras.users@gmail.com>
2024-04-23 01:34:21 +0330

    section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {NgClass, NgForOf, NgIf, NgSwitch, NgSwitchCase} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass, NgIf, NgForOf, NgSwitchCase, NgSwitch],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-learn';
+export class AppComponent implements OnInit{
+  title: string = 'my angular app';
+  num: number = 10
+  isDisable: boolean = false;
+
+  isActive: boolean = true;
+
+  style = {
+    'font-size': '70px'
+  }
+
+  names : string[] = ['ali', 'mohamad', 'reza', 'hosein'];
+
+  userStatus: number = 2;
+
+  hasError: boolean = false;
+  errorMessage: string = '';
+
+  isShow = false
+  public onChange(event: Event): void {
+    this.title = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.errorMessage = 'service has error'
+      this.hasError = true;
+    }, 8000)
+  }
+
+
+
 }
